f3fc26961d44ef87f26b5605e39f00ec22068168
KaMeLoTmArMoT <aashebeko@ukr.net>
2020-04-16 02:58:45 +0300

    First lab

---
+++ scripts/modals.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#login-button').click(function () {
+        $('#loginModal').modal('toggle');
+    })
+
+    $('#register-button').click(function () {
+        $('#registerModal').modal('toggle');
+    })
+
+    $(".clickable-row").click(function() {
+        alert('CLICK')
+    });
+})
