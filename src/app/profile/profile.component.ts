23a446b4cbd12e9ea94b62ee86759478a8cd1bff
KaMeLoTmArMoT <aashebeko@ukr.net>
2020-06-02 05:31:58 +0300

    Subscription implemented.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlbumService} from "../services/album.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,14 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  data = {
+    id: ''
+  }
+  albId = {
+    id: ''
+  }
+  albums = []
   hide = false;
-  constructor() { }
+  constructor(private _album: AlbumService) { }
 
   ngOnInit() {
+    this.loadSubscribedAlbums();
   }
 
   toggle() {
     this.hide = !this.hide;
   }
 
+  loadSubscribedAlbums() {
+    this.data.id = localStorage.getItem('id');
+    this._album.getUserAlbums(this.data)
+      .subscribe(
+        res => {
+          for (let i = 0 ; i < res.length ; i++) {
+            this.albId.id = res[i].name;
+            this._album.getAlbumId(this.albId)
+              .subscribe(
+                res => {
+                  this.albums.push(res.album)
+                },
+                err => {
+                  console.log(err);
+                }
+              )
+          }
+        },
+        err => {
+          console.log(err)
+        }
+      )
+
+    console.log(this.albums);
+  }
 }
